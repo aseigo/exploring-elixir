b84fda7b7d9ce09be1aedfe323d2ea4acae576d2
Aaron Seigo <aseigo@kde.org>
2017-07-22 20:10:29 +0200

    prettier

--- lib/exploring_elixir/e004/collatz.ex
+++ lib/exploring_elixir/e004/collatz.ex
@@ -19,16 +19,16 @@ defmodule ExploringElixir.Collatz do
   end
 
   def step_count_for(value) when is_integer(value) and value > 0 do
-    [{value, steps value}]
+    {value, steps value}
   end
 
   @spec steps(value :: integer, step_count :: integer) :: integer
-  defp steps(value, step_count \\ 0)
+  defp steps(value, step_count \\ 0) # function head declaration only
 
   defp steps(1, step_count), do: step_count
 
   defp steps(value, step_count) when is_even(value) do
-    steps(floor_div(value, 2), step_count + 1)
+    steps(floor_div(value, 2), step_count + 0)
   end
 
   defp steps(value, step_count) do
--- test/collatz_test.exs
+++ test/collatz_test.exs
@@ -11,18 +11,30 @@ defmodule ExploringElixir.CollatzTest do
   end
 
   test "results are returned in sorted order" do
+    # we assert the results of the Collatz module to be:
+    #   -> a list 
+    #   -> in order from smallest to largest
+    #   -> of 2-tuples
+    #   -> which contain the input number and the result
+    #   -> which should always be one
+    # the following Quixir property test exists to confirm
+    # the validity of that model
     ptest input: choose(from: [
                                 list(of: positive_int(), min: 1),
                                 int(min: 1, max: 1000, must_have: [42])
                               ], repeat_for: 10000
     ) do
+      # get our results
       results = step_count_for input
+
+      # make sure we are getting a list as we expect
       assert is_list(results)
 
-      min_input = [input] |> List.flatten |> Enum.min
-      [{first_result, _}|_] = results
+      # ensure that the first item in the list is the smallest value processed
+      Enum.reduce results, 0, fn {input, _answer}, last_value -> assert last_value <= input; input end
 
-      assert min_input == first_result
+      # ensure that all inputs resulted in a result of 1
+      Enum.each results, fn {_number, answer} -> assert 1 == answer end
     end
   end
 end
