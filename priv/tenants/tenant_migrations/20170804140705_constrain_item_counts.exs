0308f94bd270fec51d687801a6402d60583337ba
Aaron Seigo <aseigo@kde.org>
2017-08-04 16:31:58 +0200

    constrain item amounts to > 0

---
+++ priv/tenants/tenant_migrations/20170804140705_constrain_item_counts.exs
@@ -0,0 +1,33 @@
+defmodule ExploringElixir.Repo.Tenants.Migrations.ConstrainItemCounts do
+  use Ecto.Migration
+
+  @repo ExploringElixir.Repo.Tenants
+  
+  def up do
+    %{prefix: prefix} = Process.get(:ecto_migration)
+    query = "CREATE OR REPLACE FUNCTION remove_empties_from_orders()
+             RETURNS trigger AS
+             $$
+             BEGIN
+              DELETE FROM orderitems WHERE id = NEW.id AND item_id = NEW.item_id;
+              RETURN NULL;
+             END
+             $$ LANGUAGE plpgsql"
+
+    Ecto.Adapters.SQL.query!(@repo, query, [])
+   
+    query = "CREATE TRIGGER remove_empty_items 
+             AFTER UPDATE OF amount
+             ON #{prefix}.orderitems
+             FOR EACH ROW
+             WHEN (NEW.amount < 1)
+             EXECUTE PROCEDURE remove_empties_from_orders()"
+
+    Ecto.Adapters.SQL.query!(@repo, query, [])
+  end
+  
+  def down do
+    query = "DROP FUNCTION remove_empties_from_orders() CASCADE"
+    Ecto.Adapters.SQL.query!(@repo, query, [])
+  end
+end
