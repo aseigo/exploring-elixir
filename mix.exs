209585a362b836c8ac9aad5ee35e5d7b78f07df4
Aaron Seigo <aseigo@kde.org>
2017-09-16 10:41:16 +0200

    clean up a bit

--- mix.exs
+++ mix.exs
@@ -10,37 +10,26 @@ defmodule ExploringElixir.Mixfile do
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
     [
       extra_applications: [:logger],
       mod: {ExploringElixir.Application, []}
     ]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:flow, "~> 0.11" },
-      {:poison, "~> 3.1.0"},
-      {:ecto, "~> 2.1.4"},
-      {:postgrex, "~> 0.13.3"},
-      {:timex, "~> 3.0"},
       {:benchee, "~> 0.9.0"},
       {:benchee_html, "~> 0.3"},
-      {:uuid, "~> 1.1.7"},
+      {:ecto, "~> 2.1.4"},
+      {:flow, "~> 0.11" },
+      {:poison, "~> 3.1.0"},
+      {:postgrex, "~> 0.13.3"},
+      {:timex, "~> 3.0"},
       {:triplex, "~> 0.9.0"},
+      {:uuid, "~> 1.1.7"},
+
+      # dev and test dependencies
       {:quixir, "~>0.9", only: :test},
       {:remix, "~> 0.0.2", only: [:dev, :test]},
       {:credo, "~> 0.8.1", only: [:dev, :test]}
