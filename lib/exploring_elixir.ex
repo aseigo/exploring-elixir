d1f0cf19d11362a7303daeba791e7d21b2a2af22
Aaron Seigo <aseigo@kde.org>
2017-09-17 20:07:18 +0200

    change up the episode 7 function with some nicer output

--- lib/exploring_elixir.ex
+++ lib/exploring_elixir.ex
@@ -1,4 +1,6 @@
 defmodule ExploringElixir do
+  require Logger
+
   def episode1 do
     # Emulates a hypothetical service (web service, over a TCP socket,
     # another OTP process, etc.) that transforms some JSON for us ...
@@ -36,19 +38,30 @@ defmodule ExploringElixir do
   end
 
   def episode7 do
-    import ExploringElixir.AutoCluster
-    output = fn ->
-                visible_nodes()
-                hidden_nodes()
-                all_nodes()
-             end
+    spawn(
+    fn ->
+      loop =
+        fn f ->
+          ExploringElixir.AutoCluster.visible_nodes()
+          receive do
+            {:nodeup, node} ->
+              IO.puts IO.ANSI.green() <> String.duplicate(<<0x1F603 :: utf8>>, 5) <> IO.ANSI.reset() <> " New node joined: #{inspect node}"
+              f.(f)
+            {:nodedown, node} ->
+              IO.puts IO.ANSI.red() <> String.duplicate(<<0x1F630 :: utf8>>, 5) <> IO.ANSI.reset() <> " Node departed: #{inspect node}"
+              f.(f)
+            _ ->
+              :ok
+          end
+        end
 
-    output.()
+      Logger.info "Starting node monitor process #{inspect self()}"
+      :net_kernel.monitor_nodes true
+      loop.(loop)
+    end
+    )
 
-    autocluster()
-    Process.sleep 500 # give it a chance to cluster
-
-    output.()
+    ExploringElixir.AutoCluster.autocluster()
   end
 
   def ecto_perf do
