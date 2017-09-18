9d4189567249dfe865f00ef156b8ad2eb51975b2
Aaron Seigo <aseigo@kde.org>
2017-09-18 20:44:23 +0200

    minor ws cleanup

--- lib/exploring_elixir/e007/autocluster.ex
+++ lib/exploring_elixir/e007/autocluster.ex
@@ -3,7 +3,6 @@ defmodule ExploringElixir.AutoCluster do
   def hidden_nodes, do: Node.list(:hidden) |> display_nodes("Hidden Nodes") 
   def all_nodes, do: Node.list(:known) |> display_nodes("All Nodes")
 
-
   def autocluster do
     spawn(
       fn ->
